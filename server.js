65f99e6441f51cb3a8b293b17c58e9fd3f60fd07
dgeisert <memphis.gatech@gmail.com>
2017-06-12 13:02:36 -0700

    trying express

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(80, function(){
-    console.log('Server running on 80...');
+// Node.js notation for importing packages
+var express = require('express');
+
+// Spin up a server
+var app = express();
+
+// Serve static files from the main build directory
+app.use(express.static(__dirname + '/build/bundled'));
+
+// Render index.html on the main page, specify the root
+app.get('/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
+
+// Tell the app to listen for requests on port 3000
+app.listen(80, function () {
+    console.log('Portfolio app listening on port 80!');
 });
