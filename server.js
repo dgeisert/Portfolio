9e942d42335ba062333fecad335b89e8b2f57865
dgeisert <memphis.gatech@gmail.com>
2017-06-12 13:10:56 -0700

    trying express

--- server.js
+++ server.js
@@ -8,29 +8,7 @@ var app = express();
 app.use(express.static(__dirname + '/build/default'));
 
 // Render index.html on the main page, specify the root
-app.get('/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-});
-app.get('/view1/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-    res.sendFile("src/my-view1.html", { root: '.' });
-});
-app.get('/view2/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-});
-app.get('/view3/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-});
-app.get('/view4/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-});
-app.get('/view5/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-});
-app.get('/view6/', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
-});
-app.get('/single-project/', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile("index.html", { root: '.' });
 });
 
