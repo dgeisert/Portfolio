40bbd680b7a9906bb02f82ca436b002d6112ac5e
dgeisert <memphis.gatech@gmail.com>
2017-06-12 13:05:26 -0700

    trying express

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ app.use(express.static(__dirname + '/build/bundled'));
 app.get('/', function (req, res) {
     res.sendFile("index.html", { root: '.' });
 });
+app.get('/view1/', function (req, res) {
+    res.sendFile("src/my-view1.html", { root: '.' });
+});
+app.get('/view2/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
+app.get('/view3/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
+app.get('/view4/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
+app.get('/view5/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
+app.get('/view6/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
+app.get('/single-project/', function (req, res) {
+    res.sendFile("index.html", { root: '.' });
+});
 
 // Tell the app to listen for requests on port 3000
 app.listen(80, function () {
